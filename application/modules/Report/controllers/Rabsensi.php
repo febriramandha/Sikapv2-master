0678287a2d4b86fdcf8d1f9a80a4f7e1cb7b1c7c
rianreski <rianreski13@gmail.com>
2020-01-24 09:10:02 +0700

    commit 1

---
+++ application/modules/Report/controllers/Rabsensi.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* Created By: Rian Reski A
+* 2019
+*/
+
+class Rabsensi extends App_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->_init();
+		$this->breadcrumbs->push('Laporan Kehadiran', 'mnglkh/schlkh');
+		$this->data['title'] = "Laporan Umum";
+		$this->load->model('m_instansi');
+	}
+
+	private function _init()
+	{
+		$this->output->set_template('app');
+		$this->load->css('public/themes/plugin/datepicker/css/bootstrap-datepicker.css');
+    	$this->load->js('public/themes/plugin/datepicker/js/bootstrap-datepicker.js');
+	}
+
+	public function index()
+	{
+		$this->data['sub_title']  = "Laporan Kehadiran";
+		$this->data['breadcrumb'] = $this->breadcrumbs->show();
+		$this->load->view('rabsensi/v_index', $this->data);
+	}
+
+}
+
+/* End of file Rabsensi.php */
+/* Location: ./application/modules/Report/controllers/Rabsensi.php */
