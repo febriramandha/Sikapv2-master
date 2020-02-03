dc38803b157f3dce29a474206a078b2bfaa8ae44
rianreski <rianreski13@gmail.com>
2020-02-04 00:58:42 +0700

    commit 1

--- application/modules/report/controllers/Rtibadah.php
+++ application/modules/report/controllers/Rtibadah.php
@@ -35,6 +35,76 @@ class Rtibadah extends App_Controller {
 		$this->load->view('rtibadah/v_index', $this->data);
 	}
 
+	public function AjaxGet()
+	{
+		$this->output->unset_template();
+		$this->mod = $this->input->get('mod');
+		if ($this->mod == "DataPegawai") {
+			    $dept_id 		= decrypt_url($this->input->get('instansi'),'instansi');
+        		$data_pegawai 	= $this->m_user->GetUserAllAktifDept($dept_id)->result();
+        		$res='';
+        		foreach ($data_pegawai as $row ){
+		          		$res.= "<option value='".encrypt_url($row->id,'user_id_ibadah')."'>$row->nama ($row->nip)</option>";
+		          }
+		        $this->output->set_output($res);
+		} elseif ($this->mod == "CekAgama") {
+				$data_agama ='';
+				if ($this->input->get('pegawai')) {
+					$data_agama = $this->db->select('agama_id')->get_where('sp_pegawai',['user_id' => decrypt_url($this->input->get('pegawai'),'user_id_ibadah')])->row();
+				}
+				
+				$agama_id = 1;
+				if ($data_agama) {
+						$agama_id = $data_agama->agama_id;
+				}
+
+				$data = array('agama_id' => $agama_id, );
+
+				$this->result = array('status' => true,
+			    			   		   'message' => 'Berhasil mengabil data',
+			    			   		   'data' => $data);
+				$this->output->set_output(json_encode($this->result));
+		}
+		
+	}
+
+	public function ibadahJson()
+	{
+		$this->output->unset_template();
+		
+		$rank1 = format_tgl_eng($this->input->post('rank1'));
+		$rank2 = format_tgl_eng($this->input->post('rank2'));
+		$user_id = decrypt_url($this->input->post('pegawai'),'user_id_ibadah');
+		$agama_id = $this->input->post('agama_id');
+		$this->load->library('datatables');
+		if ($agama_id != 1) {
+			 $this->datatables->select('id, tgl_ibadah, kegiatan, tempat')
+        	->from('ibadah_nonmus')
+        	->order_by('tgl_ibadah','desc')
+        	->add_column('tgl_ibadah_tabel','$1','tglInd_hrtabel(tgl_ibadah)')
+        	->add_column('tgl_ibadah_tabel','$1','tglInd_hrtabel(tgl_ibadah)')
+	        				->add_column('tempat1','$1','kegiatan')
+	        				->add_column('tempat2','$1','tempat');
+		}else {
+			$this->datatables->select('a.id, tgl_ibadah, ibadah_id, t_zuhur, t_ashar, b.nama as ket')
+	        				->from('ibadah_muslim a')
+	        				->join('ibadah b','a.ibadah_id=b.id','left')
+	        				->order_by('tgl_ibadah','desc')
+	        				->add_column('tgl_ibadah_tabel','$1','tglInd_hrtabel(tgl_ibadah)')
+	        				->add_column('tempat1','$1','t_zuhur')
+	        				->add_column('tempat2','$1','t_ashar');
+		}
+    	if ($rank1 && $rank2) {
+    			$this->datatables->where("tgl_ibadah::date BETWEEN '$rank1' and '$rank2'", NULL, FALSE );
+    	}
+    	if ($user_id) {
+    		$this->datatables->where('user_id', $user_id);
+    	}else {
+    		$this->datatables->where('user_id', '0');
+    	}
+        return $this->output->set_output($this->datatables->generate());
+	}
+
 
 
 }
---
+++ application/modules/report/views/rtibadah/v_index.php
@@ -0,0 +1,234 @@
+<!-- Basic table -->
+<div class="card">
+	<div class="card-header bg-white header-elements-inline py-2">
+		<h5 class="card-title">Laporan LKH</h5>
+		<div class="header-elements">
+			<div class="list-icons">
+				<a class="list-icons-item" data-action="collapse"></a>
+			</div>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="form-group row">
+			<label class="col-form-label col-lg-2">Instansi <span class="text-danger">*</span></label>
+			<div class="col-lg-10">
+				<div class="form-group">
+					<select class="form-control select-search" name="instansi"> 
+						<?php foreach ($instansi as $row) { ?>
+							<option value="<?php echo encrypt_url($row->id,'instansi') ?>"><?php echo '['.$row->level.']'.carakteX($row->level, '-','|').filter_path($row->path_info)." ".strtoupper($row->dept_name) ?></option>
+						<?php } ?>
+					</select> 
+				</div>
+			</div>
+		</div>
+		<div class="form-group row">
+	        <label class="col-form-label col-lg-2">Pegawai<span class="text-danger">*</span>
+	       	 	<i class="icon-spinner2 spinner" style="display: none" id="spinner_pegawai"></i>
+	    	</label>
+	        <div class="col-lg-10">
+	          <div class="form-group">
+	           <select class="form-control select-search" name="pegawai" id="pegawai"> 
+	           		
+	          </select> 
+	        </div>
+	      </div>
+	    </div>
+		<div class="form-group row">
+			<label class="col-form-label col-lg-2"> Rentang Waktu <span class="text-danger">*</span></label>
+			<div class="col-lg-4">
+				<div class="form-group-feedback form-group-feedback-left">
+					<div class="form-control-feedback">
+						<i class="icon-pencil3"></i>
+					</div>
+					<input type="text" name="rank1" class="form-control datepicker" placeholder="dari tanggal" >
+				</div>
+			</div>
+			<div class="col-lg-1">
+				<div class="form-group">
+					<span>s/d</span>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="form-group-feedback form-group-feedback-left">
+					<div class="form-control-feedback">
+						<i class="icon-pencil3"></i>
+					</div>
+					<input type="text" name="rank2" class="form-control datepicker" placeholder="sampai tanggal" >
+				</div>
+			</div>
+		</div>
+		<input type="hidden" name="agama_id" value="0">
+	    <div class="text-left offset-lg-2" >                
+			<button type="submit" class="btn btn-sm btn-info result" id="kalkulasi">Kalkulasi <i class="icon-search4 ml-2"></i></button>
+			<button class="btn btn-sm bg-success-400 legitRipple pt-1 pb-1" id="cetak">
+				<span><i class="icon-printer mr-2"></i> Cetak</span>
+			</button> 
+			<i class="icon-spinner2 spinner" style="display: none" id="spinner"></i>	
+		</div>	
+		<div class="table-responsive">
+			<table id="datatable" class="table table-sm table-hover table-bordered">
+				<thead>
+					<tr class="table-active">
+						<th width="1%">No</th>
+						<th class="text-nowrap" width="1%">Tanggal</th>
+						<th class="text-center">
+						<i class="icon-spinner2 spinner spinner_tempat" style="display: none" ></i>
+							<span id="zuhur">Tempat Sholat Zuhur</span></th>
+						<th class="text-center">
+						<i class="icon-spinner2 spinner spinner_tempat" style="display: none" ></i>
+							<span id="ashar">Tempat Sholat Ashar </span></th>	
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+
+$('[name="instansi"]').change(function() {
+	DataPegawai();
+})
+$(".datepicker").datepicker({
+    format: 'dd-mm-yyyy',
+    autoclose: true,
+    todayHighlight: true,
+});
+
+$('[name="pegawai"]').change(function() {
+	cek_agama()
+})
+
+$(document).ready(function(){
+	DataPegawai();
+});
+
+$('#kalkulasi').click(function() {
+	result.attr("disabled", true);
+    spinner.show();
+	table.ajax.reload();
+})
+
+function DataPegawai() {
+	var instansi = $('[name="instansi"]').val();
+	var result  = $('.result');
+	var spinner = $('#spinner_pegawai');
+	$.ajax({
+		type: 'get',
+		url: uri_dasar+'report/rtibadah/AjaxGet',
+		data: {mod:'DataPegawai',instansi:instansi},
+		dataType : "html",
+		error:function(){
+			result.attr("disabled", false);
+       		spinner.hide();
+			bx_alert('gagal menghubungkan ke server cobalah mengulang halaman ini kembali');
+		},
+		beforeSend:function(){
+			result.attr("disabled", true);
+      		spinner.show();
+		},
+		success: function(res) {
+			$('#pegawai').html(res);
+			result.attr("disabled", false);
+      		spinner.hide();
+      		cek_agama();
+		}
+	});
+	
+}
+
+function cek_agama() {
+	var pegawai_id = $('[name="pegawai"]').val()
+	var result  = $('.result');
+	var spinner = $('.spinner_tempat');
+	$.ajax({
+		type: 'get',
+		url: uri_dasar+'report/rtibadah/AjaxGet',
+		data: {mod:'CekAgama',pegawai:pegawai_id},
+		dataType : "json",
+		error:function(){
+			result.attr("disabled", false);
+       		spinner.hide();
+			bx_alert('gagal menghubungkan ke server cobalah mengulang halaman ini kembali');
+		},
+		beforeSend:function(){
+			result.attr("disabled", true);
+      		spinner.show();
+		},
+		success: function(res) {
+			if (res.status == true) {
+				if (res.data.agama_id != 1) {
+					$('#zuhur').text('Kegiatan Ibadah');
+					$('#sahar').text('Tempat Ibadah');
+				}else {
+					$('#zuhur').text('Tempat Sholat Zuhur');
+					$('#sahar').text('Tempat Sholat Ashar');
+				}
+				$('[name="agama_id"]').val(res.data.agama_id);
+			}else {
+				bx_alert(res.message);
+			}
+
+			result.attr("disabled", false);
+      		spinner.hide();
+		}
+	});
+}
+
+var result  = $('.result');
+var spinner = $('#spinner');
+ $(document).ready(function(){
+     table = $('#datatable').DataTable({ 
+      processing: true, 
+      serverSide: true, 
+      "ordering": false,
+      "searching": false,
+      language: {
+            search: '<span></span> _INPUT_',
+            searchPlaceholder: 'Cari...',
+            processing: '<i class="icon-spinner9 spinner text-blue"></i> Loading..'
+        },  
+        "lengthMenu": [[10, 25, 50, 100, 200], [10, 25, 50, 100, 200]],
+      ajax: {
+          url : uri_dasar+'report/rtibadah/ibadahJson',
+          type:"post",
+          "data": function ( data ) { 
+                data.csrf_sikap_token_name= csrf_value;
+                data.pegawai		  = $('[name="pegawai"]').val();
+                data.rank1			  = $('[name="rank1"]').val();
+                data.rank2		  	  = $('[name="rank2"]').val();
+                data.agama_id		  = $('[name="agama_id"]').val();
+              },
+           "dataSrc": function ( json ) {
+                //Make your callback here.
+                result.attr("disabled", false);
+	          	spinner.hide();
+                return json.data;
+            }  
+      },
+      "columns": [
+          {"data": "id", searchable:false},
+          {"data": "tgl_ibadah_tabel", searchable:false},
+          {"data": "tempat1", searchable:false},
+          {"data": "tempat2", searchable:false},
+      ],
+      rowCallback: function(row, data, iDisplayIndex) {
+          var info = this.fnPagingInfo();
+          var page = info.iPage;
+          var length = info.iLength;
+          var index = page * length + (iDisplayIndex + 1);
+          $('td:eq(0)', row).html(index);
+      },
+       createdRow: function(row, data, index) {
+       		$('td', row).eq(1).addClass('text-nowrap');
+        },
+
+
+  });
+   dt_componen();
+
+});
+
+</script>
