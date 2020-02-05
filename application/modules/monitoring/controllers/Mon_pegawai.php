6541b07edf32f4d99d415a5b46d4043cf6d17bf9
rianreski <rianreski13@gmail.com>
2020-02-05 15:29:34 +0700

    commit 1

--- application/modules/monitoring/controllers/Mon_pegawai.php
+++ application/modules/monitoring/controllers/Mon_pegawai.php
@@ -14,6 +14,7 @@ class Mon_pegawai extends  App_Controller {
 		$this->_init();
 		$this->breadcrumbs->push('Monitoring Pegawai', 'monitorinbg/mon-pegawai');
 		$this->data['title'] = "Monitoring";
+		$this->load->model('m_instansi');
 	}
 
 	private function _init()
@@ -26,6 +27,31 @@ class Mon_pegawai extends  App_Controller {
 		$this->data['sub_title']  = "Pegawai";
 		$this->data['breadcrumb'] = $this->breadcrumbs->show();
 		$this->load->view('mon_pegawai/v_index', $this->data);
+	}
+
+	public function indexJson()
+	{
+		$this->output->unset_template();
+		$dept_id = $this->m_instansi->GetAdminDept($this->session->userdata('tpp_dept_id'));
+		$instansi = decrypt_url($this->input->post('instansi'),'instansi');
+		$level 	  = $this->db->select('level')->get_where('v_instansi_all', ['id' => $dept_id])->row()->level;
+		$this->load->library('datatables');
+        $this->datatables->select('a.id, a.nip, a.nama, a.dept_alias, gelar_dpn,gelar_blk,jabatan')
+        	->from('v_users_all a')
+        	->join('users_login b','a.id=b.user_id','left')
+        	->order_by('no_urut')
+        	->add_column('nama_nip','$1','nama_icon_nip(nama,gelar_dpn,gelar_blk,nip,"kepegawaian/data-pegawai/view",id)')
+        	->add_column('instansi','$1','dept_alias')
+        	->add_column('jabatan','$1','jabatan');
+		     $this->datatables->where("path_id['".$level."']='".$dept_id."'");
+        	 if ($this->input->post('search[value]')) {
+        	 	$this->db->group_start();
+		        	$this->datatables->like('lower(nama)', strtolower($this->input->post('search[value]')));
+		        	$this->datatables->or_like('lower(nip)', strtolower($this->input->post('search[value]')));
+	        	$this->db->group_end();
+	        }
+	       
+        return $this->output->set_output($this->datatables->generate());
 	}
 
 }
--- application/modules/monitoring/views/mon_pegawai/v_index.php
+++ application/modules/monitoring/views/mon_pegawai/v_index.php
@@ -2,22 +2,20 @@
 <div class="card">
 	<div class="card-body">
 		<h5 class="mb-3">Monitoring Pegawai</h5>
-
-		<form action="#">
 			<div class="input-group mb-3">
 				<div class="form-group-feedback form-group-feedback-left">
-					<input type="text" class="form-control form-control-lg" value="" placeholder="Cari nama,nip, eselon, jabatan">
+					<input type="text" name="pegawai" class="form-control form-control-lg" value="" placeholder="Cari nama,nip, eselon, jabatan">
 					<div class="form-control-feedback form-control-feedback-lg">
 						<i class="icon-search4 text-muted"></i>
 					</div>
 				</div>
 
 				<div class="input-group-append">
-					<button type="submit" class="btn btn-primary btn-lg">Cari</button>
+					<button type="submit" class="btn btn-primary btn-sm	" id="cari"><i class="icon-search4 mr-2"></i> Cari</button>
 				</div>
 			</div>
-		</form>
 
+		<i class="icon-spinner2 spinner icon-2x" style="display: none" id="spinner"></i>
 		<div class="table-responsive">
 			<table id="datatable" class="table table-sm table-hover table-bordered">
 				<thead>
@@ -36,3 +34,71 @@
 	</div>
 </div>
 <!-- /search field -->
+<script type="text/javascript">
+var result  = $('.result');
+var spinner = $('#spinner');
+
+$('#cari').click(function() {
+	result.attr("disabled", true);
+    spinner.show();
+    $('#cari').hide();
+	table.ajax.reload();
+})
+
+
+$(document).ready(function(){
+     table = $('#datatable').DataTable({ 
+	    processing: true, 
+	    serverSide: true, 
+	    "ordering": false,
+	    "searching": false,
+	    language: {
+            search: '<span></span> _INPUT_',
+            searchPlaceholder: 'Cari...',
+            processing: '<i class="icon-spinner9 spinner text-blue"></i> Loading..'
+        },  
+        "lengthMenu": [[10, 25, 50, 100, 200], [10, 25, 50, 100, 200]],
+	    ajax: {
+	        url : uri_dasar+'monitoring/mon-pegawai/indexJson',
+	        type:"post",
+	        "data": function ( data ) {	
+        				data.csrf_sikap_token_name= csrf_value;
+        				data.cari  = $('[name="pegawai"]').val();
+	            },
+	        beforeSend:function(){
+					result.attr("disabled", true);
+					$('#cari').hide();
+		      		spinner.show();
+			},
+			"dataSrc": function ( json ) {
+                //Make your callback here.
+                result.attr("disabled", false);
+	          	spinner.hide();
+	          	$('#cari').show();
+                return json.data;
+            } 
+	    },
+	    "columns": [
+	        {"data": "id", searchable:false},
+	        {"data": "nama_nip", searchable:false},
+	        {"data": "instansi", searchable:false},
+	        {"data": "jabatan", searchable:false},
+	    ],
+	    rowCallback: function(row, data, iDisplayIndex) {
+	        var info = this.fnPagingInfo();
+	        var page = info.iPage;
+	        var length = info.iLength;
+	        var index = page * length + (iDisplayIndex + 1);
+	        $('td:eq(0)', row).html(index);
+	    },
+       createdRow: function(row, data, index) {
+           $('td', row).eq(1).addClass('text-nowrap p-1');
+           $('td', row).eq(2).addClass('text-nowrap p-2');
+           $('td', row).eq(3).addClass('p-2');
+        },
+
+
+	});
+	 dt_componen();
+});
+</script>
