c1421af6eb8b4300a48d753d0b5dfdc0bb300f97
rianreski <rianreski13@gmail.com>
2020-02-05 23:44:00 +0700

    commit 1

---
+++ application/modules/report/controllers/Rekap_tpp.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* Created By: Rian Reski A
+* 2019
+*/
+
+
+class Rekap_tpp extends App_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->_init();
+		$this->breadcrumbs->push('Rekapitulasi Kehadiran', 'report/rekap-kehadiran');
+		$this->data['title'] = "Laporan Umum";
+		$this->load->model(['m_instansi','m_pejabat_instansi','m_absen','m_sch_run']);
+	}
+
+	private function _init()
+	{
+		$this->output->set_template('app');
+		$this->load->css('public/themes/plugin/datepicker/css/bootstrap-datepicker.css');
+    	$this->load->js('public/themes/plugin/datepicker/js/bootstrap-datepicker.js');
+    	$this->load->css('public/themes/plugin/chekbox/rrcheckbox.css');
+    	$this->load->js('public/themes/material/global_assets/js/plugins/forms/selects/bootstrap_multiselect.js');
+    	$this->load->css('public/themes/plugin/toplipcss/rrtooltip/rrtooltip.css');
+	}
+
+	public function index()
+	{
+		$this->data['sub_title']  = "Rekapitulasi Kehadiran";
+		$this->data['breadcrumb'] = $this->breadcrumbs->show();
+		$this->data['instansi']	  = $this->m_instansi->GetInstasiDeptID($this->session->userdata('tpp_dept_id'))->result();
+		$this->data['laporan_tahun'] = $this->m_sch_run->GetTahun()->result();
+		$this->load->view('rekap_tpp/v_index', $this->data);
+	}
+
+
+}
+
+/* End of file Rekap_tpp.php */
+/* Location: ./application/modules/report/controllers/Rekap_tpp.php */
