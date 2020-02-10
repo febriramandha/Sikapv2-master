db242656abff66e425ea93c2e23d7765d0a14c4d
rianreski <rianreski13@gmail.com>
2020-02-10 21:03:59 +0700

    commit 1

--- application/config/config.php
+++ application/config/config.php
@@ -51,10 +51,9 @@ $config['maintenance_mode'] = FALSE;
 | a PHP script and you can easily do that on your own.
 |
 */
-$http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
-$newurl = str_replace("index.php","", $_SERVER['SCRIPT_NAME']);
-
-$config['base_url'] = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 
 /*
