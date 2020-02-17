cf71cb446b379a70ff8b0eafde1cb2bf2c0a634c
rianreski <rianreski13@gmail.com>
2020-02-17 17:43:08 +0700

    commit 1

--- application/modules/datalkh/views/lkh/v_add.php
+++ application/modules/datalkh/views/lkh/v_add.php
@@ -152,7 +152,7 @@ CKEDITOR.replaceClass = 'ckeditor';
 $('.ckeditor').each(function(e){
       CKEDITOR.replace( this.id, {  height:'80px',
               tabSpaces: 4,
-              customConfig: uri_dasar+'public/themes/plugin/ckeditor/custom/ckeditor_config_text.js' });
+              customConfig: uri_dasar+'public/themes/plugin/ckeditor/custom/ckeditor_config_text_add.js' });
 });
 
 function CKupdate(){
--- public/themes/plugin/ckeditor/custom/ckeditor_config_text_add.js
+++ public/themes/plugin/ckeditor/custom/ckeditor_config_text_add.js
@@ -5,17 +5,17 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'links', groups: [ 'links' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
 		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'others', groups: [ 'others' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
 		'/',
 		{ name: 'styles', groups: [ 'styles' ] },
 		{ name: 'colors', groups: [ 'colors' ] },
 		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	config.removeButtons = 'Subscript,Superscript,Paste,PasteText,PasteFromWord,Copy,Undo,Redo,Scayt,Link,Unlink,Anchor,Image,Table,HorizontalRule,SpecialChar,Source,Strike,RemoveFormat,BulletedList,Outdent,Indent,Blockquote,Styles,Format,About,Cut';
+	config.removeButtons = 'Subscript,Superscript,Paste,PasteText,PasteFromWord,Copy,Undo,Redo,Scayt,Link,Unlink,Anchor,Image,Table,HorizontalRule,SpecialChar,Source,Strike,RemoveFormat,BulletedList,Outdent,Indent,Blockquote,Styles,Format,About';
 	config.enterMode = CKEDITOR.ENTER_BR;
 };
