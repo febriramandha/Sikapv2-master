61df57a4435388ec186a2e1f8eeeb1dfd82c1038
rianreski <rianreski13@gmail.com>
2020-02-18 12:00:26 +0700

    commit 1

--- application/controllers/app/Dashboard.php
+++ application/controllers/app/Dashboard.php
@@ -28,12 +28,14 @@ class Dashboard extends App_Controller {
 		$this->data['user_aktif_all']	= $this->db->select('count(*)')->where('status',1)->get('users_login')->row();
 		$this->data['user_admin_all']	= $this->db->select('count(*)')->where('level',2)->or_where('level', 1)->get('users_login')->row();
 		$this->data['user_aktif_all']	= $this->db->select('count(*)')->where('status',1)->get('users_login')->row();
-		$this->data['opd_all']			= $this->db->select('count(*)')->where('parent_id',1)->get('mf_departments')->row();
-		$this->data['puskesmas_all']	= $this->db->select('count(*)')->where('parent_id',12)->where('position_order not in(1)')->get('mf_departments')->row();
-		$this->data['sekolah_all']		= $this->db->select('count(*)')->where('parent_id',43)->where('position_order not in(1)')->get('mf_departments')->row();
+		
+		// $this->data['opd_all']			= $this->db->select('count(*)')->where('parent_id',1)->get('mf_departments')->row();
+		// $this->data['puskesmas_all']	= $this->db->select('count(*)')->where('parent_id',12)->where('position_order not in(1)')->get('mf_departments')->row();
+		// $this->data['sekolah_all']		= $this->db->select('count(*)')->where('parent_id',43)->where('position_order not in(1)')->get('mf_departments')->row();
 
 		//user
-		$this->data['user_opd_all']		= $this->db->select('count(*)')->where('parent_id',1)->get('v_users_all')->row();
+		// $this->data['user_opd_all']		= $this->db->select('count(*)')->where('parent_id',1)->get('v_users_all')->row();
+
 		$this->data['instansi']	  = $this->m_instansi->GetInstasiDeptID($this->session->userdata('tpp_dept_id'))->result();
 		$this->data['laporan_tahun'] = $this->m_sch_run->GetTahun()->result();
 
