c24b365ec8e00cac7e18f64e61fb0147aceff527
rianreski <rianreski13@gmail.com>
2020-03-20 10:06:13 +0700

    commit 1

--- application/modules/datalkh/views/lkh/v_index.php
+++ application/modules/datalkh/views/lkh/v_index.php
@@ -79,7 +79,27 @@
 		</div>
 	</div>
 </div>
-
+<!-- Modal -->
+<div class="modal fade" id="exampleModalinfo" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-white py-2">
+        <h5 class="modal-title" id="exampleModalCenterTitle"><i class="icon-info22 mr-2"></i> Informasi</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      	 <p>Untuk informasi seputar aplikasi SIKAP. Silahkan bergabung ke group telegram berikut:<br>
+      	 	<a href="https://t.me/joinchat/JfIKS1PkpAzNigljE8OEzQ" target="_blank">https://t.me/joinchat/JfIKS1PkpAzNigljE8OEzQ</a></p>	
+         <!-- <button type="button" class="btn btn-primary">Klik Gabung</button> -->
+      </div>
+      <div class="modal-footer bg-white py-2">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+      </div>
+    </div>
+  </div>
+</div>
 <script type="text/javascript">
  $(".datepicker").datepicker({
     format: 'dd-mm-yyyy',
@@ -94,6 +114,7 @@ $('.readonlyjm').on('focus',function(){
  var result  = $('.result');
  var spinner = $('#spinner');
  $(document).ready(function(){
+ 	 $('#exampleModalinfo').modal('show');
      table = $('#datatable').DataTable({ 
       processing: true, 
       serverSide: true, 
