7b4ed79c2ede31972331752cbe23eaa44a71ba96
Rian Reski <54071877+rianreski@users.noreply.github.com>
2020-05-04 15:51:02 -0700

    commit 1

--- application/models/M_sch_lkh.php
+++ application/models/M_sch_lkh.php
@@ -10,10 +10,24 @@ class M_sch_lkh extends CI_Model {
 
 	public function Getsch_lkh($dept_id, $tgl)
 	{
-		$this->db->select('ket, count_inday, count_verday')
+		$cek = $this->Getsch_lkh_user($this->session->userdata('tpp_user_id'), $tgl);
+		if($cek->row()) {
+			$data_return = $cek;
+		}else {
+			$this->db->select('ket, count_inday, count_verday')
 				  ->where("$dept_id",'any(dept_id)',false)
 				  ->where("(start_date <= '$tgl' and end_date >= '$tgl')",'',false);
-		return $this->db->get('sch_lkh');
+			$data_return =  $this->db->get('sch_lkh');
+		}
+		return $data_return;	
+	}
+
+	public function Getsch_lkh_user($user_id, $tgl)
+	{
+		$this->db->select('ket, count_inday, count_verday')
+				  ->where("$user_id",'any(user_id)',false)
+				  ->where("(start_date <= '$tgl' and end_date >= '$tgl')",'',false);
+		return $this->db->get('sch_lkh_users');
 	}
 
 }
