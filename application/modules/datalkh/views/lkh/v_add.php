394c32f1426772d219eae2954c5f8fb8b7351d3e
zolvilko <zolvilko76@gmail.com>
2021-04-13 16:41:27 +0700

    Revert "comit 2"
    This reverts commit e838c7d18cf74531572bb5a17b0377b9535e9c2f.

--- application/modules/datalkh/views/lkh/v_add.php
+++ application/modules/datalkh/views/lkh/v_add.php
@@ -153,9 +153,7 @@ $date_now = date('Y-m-d');
       <div class="modal-body p-0" id="load-body-modal">
     
       </div>
-      <div class="alert alert-warning alert-dismissible mb-0 p-2">
-        <span class="font-weight-semibold">Perhatian!</span> Pengisian LKH paling lambat <span class="badge bg-danger ml-1">23.59 WIB</span> pada hari berikunya
-      </div>
+      <p>Pengisian LKH paling lambat pukul 23.59 WIB pada hari berikunya</p>
 
       <div class="modal-footer">
         <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal">Tutup</button>
