6316660d3cac7fccae85d8cc8e89c6399647b1a2
Handika Putra <47268896+handikap20@users.noreply.github.com>
2022-06-15 08:18:50 +0700

    update off button cetang pegawai

--- application/modules/mngsch/views/setsch_start/v_add.php
+++ application/modules/mngsch/views/setsch_start/v_add.php
@@ -69,8 +69,7 @@
                     <div class="input-group">
                         <span class="input-group-prepend">
                             <span class="input-group-text">
-                                <input type="checkbox" name="centang_pegawai" class="form-control-switchery" checked
-                                    data-fouc>
+                                <input type="checkbox" name="centang_pegawai" class="form-control-switchery" data-fouc>
                             </span>
                         </span>
                     </div>
